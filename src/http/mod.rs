073ea8f70f41f2aaa028014de381f771ee712dc7
Брескун <breskuni@gmail.com>
2023-11-11 17:41:18 +0700

    хоспаде, победа

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,10 +1,24 @@
 mod employee;
 mod projects;
 mod utils;
-use axum::Router;
+use axum::{
+    http::{self, Method},
+    Router,
+};
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn router() -> Router {
+    use http::header::CONTENT_TYPE;
+
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any)
+        .allow_headers([CONTENT_TYPE]);
+
     Router::new()
         .nest("/employee", employee::router())
         .nest("/projects", projects::router())
+        .layer(cors)
 }
